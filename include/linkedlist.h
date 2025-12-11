99edef2f8441f3f2531ceaf202ccaa957668a79d
XtheUnseen <xtheunscene@gmail.com>
2025-12-11 11:53:36 +0000

    Added basic linked list define in linkedlist.h

--- include/linkedlist.h
+++ include/linkedlist.h
@@ -1,1 +1,13 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
 
+typedef struct{
+    int item;
+    struct Node* next;
+}Node;
+
+struct Node* createNode(int data);
+struct Node* createList(int n);
+void traverseList(struct Node* head);
+
+#endif
