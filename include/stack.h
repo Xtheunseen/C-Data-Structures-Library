e31cc74ed737c4be5b9b53704944a8fbffb88923
XtheUnseen <xtheunscene@gmail.com>
2025-12-11 10:03:51 +0000

    Added define to stack header, created queue header

--- include/stack.h
+++ include/stack.h
@@ -1,1 +1,17 @@
+#ifndef STACK_H
+#define STACK_H
 
+typedef struct {
+    int *items;
+    int top;
+    int capacity;
+} Stack;
+
+Stack* createaStack(int capaicty);
+void push(Stack* stack, int item);
+int pop(Stack* stack);
+int peek(Stack* stack);
+int isEmpty(Stack* stack);
+void freeStack(Stack* stack);
+
+#endif 
