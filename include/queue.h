243b83dcfa1e52d66c51de7c81176e4801dfae29
XtheUnseen <xtheunscene@gmail.com>
2025-12-11 12:08:07 +0000

    Resolving commit, removed linkedlist.h from wrong directory, commiting modification in include/queue.h

--- include/queue.h
+++ include/queue.h
@@ -1,1 +1,16 @@
+#ifndef QUEUE_H
+#define QUEUE_H
 
+typedef struct {
+    int *items;
+    int front;
+    int back;
+    int capacity;
+} Queue;
+
+void enqueue(Queue* queue, int item);
+int dequeue(Queue* queue);
+int peek(Queue* queue);
+int isEmpty(Queue* queue);
+
+#endif
